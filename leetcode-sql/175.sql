80452657c2e4a0902bcf252a2d88afc6d222690e
deepakraghavendrabhat <deepak.raghavendra.bhat@outlook.com>
2023-02-16 08:38:57 +0530

    Create 175.sql

---
+++ leetcode-sql/175.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE Person (
+--   `Column Name` VARCHAR(9),
+--   `Type` VARCHAR(7)
+-- );
+
+-- INSERT INTO Person
+--   (`Column Name`, `Type`)
+-- VALUES
+--   ('PersonId', 'int'),
+--   ('FirstName', 'varchar'),
+--   ('LastName', 'varchar');
+
+-- CREATE TABLE Address (
+--   `Column Name` VARCHAR(9),
+--   `Type` VARCHAR(7)
+-- );
+
+-- INSERT INTO Address
+--   (`Column Name`, `Type`)
+-- VALUES
+--   ('AddressId', 'int'),
+--   ('PersonId', 'int'),
+--   ('City', 'varchar'),
+--   ('State', 'varchar');
+
+
+
+select * from person;
+
+  
